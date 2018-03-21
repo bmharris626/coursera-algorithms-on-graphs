3d18c138e7e3b3dd4863afb58a985e93b569af9d
bmharris626 <bmharris626@gmail.com>
2018-03-20 21:13:40 -0400

    Working Ex4-2

--- programming-assignment-4-answers/negative_cycle.py
+++ programming-assignment-4-answers/negative_cycle.py
@@ -1,12 +1,64 @@
 #Uses python3
 
 import sys
+import queue
 
+class DirectedGraph:
+
+    def __init__(self, G, C):
+        self.n = len(G)
+        self.G = G
+        self.W = C
+
+    def BellmanFord(self, s):
+        dist, prev = list(), list()
+        for i in range(self.n):
+            dist.append(float('inf'))
+            prev.append(None)
+        dist[s] = 0
+        for u in range(self.n-1):
+            for i in range(len(self.G[u])):
+                v = self.G[u][i]
+                if dist[v] > dist[u] + self.W[u][i]:
+                    dist[v] = dist[u] + self.W[u][i]
+                    prev[v] = u
+        return dist, prev
+
+"""
+6 11
+1 6 -10
+1 2 3
+2 6 8
+2 3 3
+2 5 -5
+3 5 1
+3 6 3
+3 4 2
+5 4 0
+6 5 5
+6 2 2
+"""
+
+def is_negative_cycle(G, W, distance):
+    for u in range(len(G)):
+        for i in range(len(G[u])):
+            v = G[u][i]
+            if distance[v] > distance[u] + W[u][i]: return True
+    return False
 
 def negative_cycle(adj, cost):
-    #write your code here
+    graph = DirectedGraph(adj, cost)
+    Q = queue.Queue()
+    Q.put(0)
+    while not Q.empty():
+        s = Q.get()
+        distance, prev = graph.BellmanFord(s)
+        if is_negative_cycle(adj, cost, distance): return 1
+        for i in range(len(distance)):
+            if distance[i] == float('inf'):
+                Q.put(i)
+                break
     return 0
-
 
 if __name__ == '__main__':
     input = sys.stdin.read()
