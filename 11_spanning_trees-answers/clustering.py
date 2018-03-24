f3bf513dd5f83c3cb7269e2697414eaa492116fe
bmharris626 <bmharris626@gmail.com>
2018-03-24 10:28:22 -0400

    Finished Week 5

--- 11_spanning_trees-answers/clustering.py
+++ 11_spanning_trees-answers/clustering.py
@@ -1,10 +1,45 @@
 #Uses python3
 import sys
 import math
+import heapq
+
+def distance(p1, p2):
+    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**(1/2)
+
+def find(pi, v):
+    while v != pi[v]:
+        v = find(pi, pi[v])
+    return v
+
+def merge(pi, rank, x, y, d):
+    r_x = find(pi, x)
+    r_y = find(pi, y)
+    if r_x == r_y: return
+    if rank[r_x] > rank[r_y]:
+        pi[r_y] = r_x
+    else:
+        pi[r_x] = r_y
+        if rank[r_x] == rank[r_y]: rank[r_y] += 1
 
 def clustering(x, y, k):
-    #write your code here
-    return -1.
+    n = len(x)
+    h, pi, rank, gamma = [], [], [], 0
+    c = 0
+    for i in range(n):
+        pi.append(i)
+        rank.append(0)
+        p1 = (x[i], y[i])
+        for j in range(n-1-i):
+            p2 = (x[1+i+j], y[1+i+j])
+            heapq.heappush(h, [distance(p1, p2), c, i, 1+i+j])
+            c += 1
+    while (len(h) > 0) and (n >= k):
+        d, c, u, v = heapq.heappop(h)
+        if find(pi, u) != find(pi, v):
+            merge(pi, rank, u, v, d)
+            if d > gamma: gamma = d
+            n -= 1
+    return float(gamma)
 
 
 if __name__ == '__main__':
--- 11_spanning_trees-answers/connecting_points.py
+++ 11_spanning_trees-answers/connecting_points.py
@@ -3,35 +3,44 @@
 import sys
 import heapq
 from random import randint
+import itertools
 
 class priorityQueue:
 
     def __init__(self):
         self.pq = list()
-        self.finder = dict()
+        self.entry_finder = dict()
+        self.counter = itertools.count()
+        self.REMOVED = '<removed-task>'
 
-    def add(self, task, priority=0):
-        if task in self.finder:
+    def add(self, task, priority):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
             self.remove(task)
-        entry = [priority, task]
-        self.finder[task] = entry
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
         heapq.heappush(self.pq, entry)
 
     def remove(self, task):
-        entry = self.finder.pop(task)
-        entry[0], entry[1] = float('inf'), None
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
 
     def pop(self):
-        priority, task = heapq.heappop(self.pq)
-        if task != None:
-            del self.finder[task]
-            return task
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heapq.heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
 
     def find(self, task):
-        return self.finder.get(task, None)
+        return self.entry_finder.get(task, self.REMOVED)
 
     def empty(self):
-        return len(self.finder) == 0
+        return len(self.entry_finder) == 0
 
 def distance(p1, p2):
     return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**(1/2)
@@ -53,7 +62,7 @@ def minimum_distance(x, y):
         for z in range(n):
             p2 = (x[z], y[z])
             w = distance(p1, p2)
-            if (PQ.find(z)!=None) and (cost[z]>w):
+            if (PQ.find(z)!=PQ.REMOVED) and (cost[z]>w):
                 cost[z], parent[z] = w, v
                 PQ.add(z, w)
     return float(sum(cost))
